172fe12b4c04e47fa108580ecee52735cfa92cf5
monk <liang_worker@sina.com>
2020-03-31 22:23:04 +0800

    '第一次提交代码'

---
+++ spring-security-demo/security-demo/src/main/java/com/monk/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.monk.controller;
+
+import com.monk.bean.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping
+    public List<User> queryAllUser(User user) {
+
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        logger.info("查询全部用户信息，查询到的结果集数量为:{}", users.size());
+        return users;
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(Long id) {
+        User user = new User();
+        user.setId(1L);
+        user.setUserName("jack");
+        user.setAge(18);
+        return user;
+    }
+
+    @PostMapping
+    public void createUser(User user){
+        logger.info("创建的用户信息为：{}", user);
+    }
+
+    @PutMapping("/{id}")
+    public void updateUser(User user){
+        logger.info("更新后的用户信息为：{}", user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(Long id){
+        logger.info("删除了ID为{}的用户", id);
+    }
+}
