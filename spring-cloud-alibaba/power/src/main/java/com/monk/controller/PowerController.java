4f82cb13f337aded0106ce7106feefa506fc1360
monk <459506421@qq.com>
2021-03-10 00:04:48 +0800

    sentinel+feign整合提交

--- spring-cloud-alibaba/power/src/main/java/com/monk/controller/PowerController.java
+++ spring-cloud-alibaba/power/src/main/java/com/monk/controller/PowerController.java
@@ -1,6 +1,7 @@
 package com.monk.controller;
 
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -11,18 +12,23 @@ import java.util.concurrent.TimeUnit;
 public class PowerController {
 
     @GetMapping("/getPower.do")
+    @SentinelResource(value = "getPower.do", fallback = "fallbackMethod")
     public Map<String, Object> getPower(String name) {
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("code", "200");
         result.put("data", "power");
-        if(null ==  name){
-            // throw new RuntimeException("mock exception");
-            // try {
-            //     TimeUnit.SECONDS.sleep(10);
-            // } catch (InterruptedException e) {
-            //     e.printStackTrace();
-            // }
+        if("tom".equals(name)){
+            throw new RuntimeException("mock exception:" + name);
+        }else if("jack".equals(name)){
+            throw new RuntimeException("mock exception:" + name);
         }
+        return result;
+    }
+
+    public Map<String, Object> fallbackMethod(String name, Throwable throwable) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("code", "500");
+        result.put("data", throwable.getMessage());
         return result;
     }
 }
